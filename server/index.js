ad2e9c517bfffd206261b2ef6d679d232723fb95
Luis Javier Canto Hurtado <javier.canto.h@gmail.com>
2021-10-06 19:51:32 -0500

    Connection to MongoDB Atlas, Mongoose and todo model

--- server/index.js
+++ server/index.js
@@ -1,20 +1,36 @@
 // Requires
 const express = require('express');
 //var cors = require('cors');
-//var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // import routes
-
 // create express app
 const app = express();
 //app.use(express.json());
 // Use Cors
 //app.use((cors));
 
+// dotenv for mongo db atlas
+require('dotenv').config();
+
+
 //testing
 app.get('/', (req, res) => {
     res.send("welcome to our todos api...")
 })
 
+const connection_string = process.env.CONNECTION_STRING
 // port
-const PORT = 3000;
-app.listen(PORT,() => console.log("Express server is running on port " + PORT));
+const port = process.env.PORT || 5000;
+app.listen(port,() => console.log("Express server is running :) on port " + port));
+
+// Mongoose connection 
+mongoose
+  .connect(connection_string, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB connection established :)"))
+  .catch((error) => console.error("MongoDB connection failed: ", error.message));
+
