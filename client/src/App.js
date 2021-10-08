006cd38ee95e319bb3d7f3ca679503f0706b715d
Luis Javier Canto Hurtado <javier.canto.h@gmail.com>
2021-10-08 01:04:55 -0500

    Navbar material ui

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,21 @@ import Todos from './components/todos/Todos';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import NavBar from './components/navbar/Navbar';
+// material ui
+import { Container } from '@material-ui/core';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Container maxWidth = "md">
           <NavBar />
             <Switch>
               <Route path="/signin" component={SignIn} />
               <Route path="/signup" component={SignUp} />
               <Route path="/" component={Todos} />
             </Switch>
+        </Container>
       </BrowserRouter>
     </>
   );
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,11 +1,86 @@
 import React from 'react';
+// material ui components
+import { AppBar, Typography, Toolbar, Button } from "@material-ui/core";
+// material ui styling
+import { makeStyles } from "@material-ui/core/styles";
+// react router dom
+import { Link, useHistory } from "react-router-dom";
+
+// using makeStyles
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    authButton: {
+      right: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    linkStyle: {
+      textDecoration: "none",
+      color: "#fafafa",
+    },
+  }));
 
 const Navbar = () => {
-    return(
-        <div>
-            <h2>Navbar</h2>
-        </div>
-    );
+    const classes = useStyles();
+    const history = useHistory();
+
+    const handleSignOut = () => {
+        history.push("/signin");
+    };
+
+    return (
+        <>
+          <div className={classes.root}>
+            <AppBar position="static" color="primary">
+                <Toolbar>
+                    <Typography variant="h4" className={classes.title}>
+                        <Link className={classes.linkStyle} to="/">
+                        toDoApp;
+                        </Link>
+                    </Typography>
+                    <>
+                        <Typography variant="subtitle2" className={classes.title}>
+                            Logged in as ...?
+                        </Typography>
+                        <Button
+                            edge="end"
+                            color="inherit"
+                            className={classes.authButton}
+                            onClick={() => handleSignOut()}
+                        >
+                            <Link className={classes.linkStyle} to="/">
+                                SignOut
+                            </Link>
+                        </Button>
+                    </>
+                    <>
+                        <Button
+                            edge="end"
+                            color="inherit"
+                            className={classes.authButton}
+                        >
+                            <Link className={classes.linkStyle} to="/signin">
+                                SignIn
+                            </Link>
+                        </Button>
+                        <Button
+                            edge="end"
+                            color="inherit"
+                            className={classes.authButton}
+                        >
+                            <Link className={classes.linkStyle} to="/signup">
+                                SignUp
+                            </Link>
+                        </Button>
+                    </>
+                </Toolbar>
+            </AppBar>
+          </div>
+        </>
+      );
 }
 
 export default Navbar;
