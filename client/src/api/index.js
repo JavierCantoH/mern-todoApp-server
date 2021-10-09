1658579454ef693fa1c4797fce51c4903305e895
Luis Javier Canto Hurtado <javier.canto.h@gmail.com>
2021-10-08 21:29:17 -0500

    Setting up client folder for deploy after server folder was deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,9 @@
 // hit our backend api
-export const url = "http://localhost:5000/api";
+
+// url testing
+// export const url = "http://localhost:5000/api";
+// url when backend has been published, dont forget about the api at the end
+export const url = "https://mean-stack-todo-app.herokuapp.com/api";
 
 // function to set the headers of our req with a key for auth
 export const setHeaders = () => {
